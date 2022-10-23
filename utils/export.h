8042708200cc6a052e347426074e6a0f9972fb4f
Max Seidenstücker <max.seidenstuecker@gmail.com>
2022-10-23 17:11:59 +0200

    Fix export to have proper UVs and vertex normals

--- utils/export.h
+++ utils/export.h
@@ -13,62 +13,66 @@ namespace Utils
           inst = new Exporter("LevelObjects");
         }
 
-      // bool exportPackedMeshToObj(const PackedMesh& mesh, const std::string& file, const std::string& name = "Object")
-      // {
-      //   FILE* f = fopen((file + ".obj").c_str(), "w");
-      //   if (!f)
-      //     return false;
-      //   fputs("# File exported by ZenLib\n", f);
-      //   fputs(("o " + name + "\n").c_str(), f);
-      //   // Write positions
-      //   for (auto& v : mesh.vertices)
-      //     fputs(("v " + std::to_string(v.Position.x) + " " + std::to_string(v.Position.y) + " " + std::to_string(-v.Position.z) + "\n").c_str(), f);
-      //   // Write texture coords
-      //   for (auto& v : mesh.vertices)
-      //     fputs(("v " + std::to_string(v.TexCoord.x) + " " + std::to_string(v.TexCoord.y) + "\n").c_str(), f);
-      //   // Write normals
-      //   for (auto& v : mesh.vertices)
-      //     fputs(("v " + std::to_string(v.Normal.x) + " " + std::to_string(v.Normal.y) + " " + std::to_string(v.Normal.z) + "\n").c_str(), f);
-      //   fputs("s off\n", f);
-      //   // Write faces
-      //   for (auto& s : mesh.subMeshes) {
-      //     fputs(("g " + s.material.matName + "\n").c_str(), f);
-      //     fputs(("usemtl " + s.material.matName + "\n").c_str(), f);
-      //     for (size_t i = 0; i < s.indices.size(); i += 3) {
-      //       uint32_t idx[] = {s.indices[i], s.indices[i + 1], s.indices[i + 2]};
-      //       std::string f1 = std::to_string(idx[0] + 1) + "/" + std::to_string(idx[0] + 1) + "/" + std::to_string(idx[0] + 1);
-      //       std::string f2 = std::to_string(idx[1] + 1) + "/" + std::to_string(idx[1] + 1) + "/" + std::to_string(idx[1] + 1);
-      //       std::string f3 = std::to_string(idx[2] + 1) + "/" + std::to_string(idx[2] + 1) + "/" + std::to_string(idx[2] + 1);
-      //       fputs(("f " + f1 + " " + f2 + " " + f3 + "\n").c_str(), f);
-      //       }
-      //     }
-      //   fclose(f);
-      //
-      //   // Write mtl
-      //   FILE* mf = fopen((file + ".mtl").c_str(), "w");
-      //   if (!mf)
-      //     return false;
-      //   for (auto& s : mesh.subMeshes) {
-      //     fputs(("newmtl " + s.material.matName + "\n").c_str(), mf);
-      //     fputs(("map_Kd " + s.material.texture + "\n").c_str(), mf);
-      //     ZMath::float4 color;
-      //     color.fromABGR8(s.material.color);
-      //     fputs(("Kd " + std::to_string(color.x) + " " + std::to_string(color.y) + " " + std::to_string(color.z) + "\n").c_str(), mf);
-      //     fputs(("Ka " + std::to_string(color.x) + " " + std::to_string(color.y) + " " + std::to_string(color.z) + "\n").c_str(), mf);
-      //     fputs("\n", mf);
-      //     }
-      //   fclose(mf);
-      //   return true;
-      // }
+      bool exportPackedMeshToObj(const PackedMesh& mesh, const std::string& file, const std::string& name = "Object")
+      {
+        if(std::find(fileNames.begin(),fileNames.end(),file)!=fileNames.end()) // only export once
+          return true;
+        FILE* f = fopen((file + ".obj").c_str(), "w");
+        if (!f)
+          return false;
+        fileNames.emplace_back(file);
+        fputs("# File exported by ZenLib\n", f);
+        fputs(("o " + name + "\n").c_str(), f);
+        // Write positions
+        for (auto& v : mesh.vertices)
+          fputs(("v " + std::to_string(v.Position.x) + " " + std::to_string(v.Position.y) + " " + std::to_string(-v.Position.z) + "\n").c_str(), f);
+        // Write texture coords
+        for (auto& v : mesh.vertices)
+          fputs(("vt " + std::to_string(v.TexCoord.x) + " " + std::to_string(v.TexCoord.y) + "\n").c_str(), f);
+        // Write normals
+        for (auto& v : mesh.vertices)
+          fputs(("vn " + std::to_string(v.Normal.x) + " " + std::to_string(v.Normal.y) + " " + std::to_string(v.Normal.z) + "\n").c_str(), f);
+        fputs("s off\n", f);
+        // Write faces
+        for (auto& s : mesh.subMeshes) {
+          fputs(("g " + s.material.matName + "\n").c_str(), f);
+          fputs(("usemtl " + s.material.matName + "\n").c_str(), f);
+          for (size_t i = 0; i < s.indices.size(); i += 3) {
+            uint32_t idx[] = {s.indices[i], s.indices[i + 1], s.indices[i + 2]};
+            std::string f1 = std::to_string(idx[0] + 1) + "/" + std::to_string(idx[0] + 1) + "/" + std::to_string(idx[0] + 1);
+            std::string f2 = std::to_string(idx[1] + 1) + "/" + std::to_string(idx[1] + 1) + "/" + std::to_string(idx[1] + 1);
+            std::string f3 = std::to_string(idx[2] + 1) + "/" + std::to_string(idx[2] + 1) + "/" + std::to_string(idx[2] + 1);
+            fputs(("f " + f1 + " " + f2 + " " + f3 + "\n").c_str(), f);
+            }
+          }
+        fclose(f);
+      
+        // Write mtl
+        FILE* mf = fopen((file + ".mtl").c_str(), "w");
+        if (!mf)
+          return false;
+        for (auto& s : mesh.subMeshes) {
+          fputs(("newmtl " + s.material.matName + "\n").c_str(), mf);
+          fputs(("map_Kd " + s.material.texture + "\n").c_str(), mf);
+          ZMath::float4 color;
+          color.fromABGR8(s.material.color);
+          fputs(("Kd " + std::to_string(color.x) + " " + std::to_string(color.y) + " " + std::to_string(color.z) + "\n").c_str(), mf);
+          fputs(("Ka " + std::to_string(color.x) + " " + std::to_string(color.y) + " " + std::to_string(color.z) + "\n").c_str(), mf);
+          fputs("\n", mf);
+          }
+        fclose(mf);
+        return true;
+      }
 
       bool exportPackedMeshToLevelObject(const ZenLoad::PackedMesh& mesh, const std::string& file, const std::string& name = "Object")
       {
-        if(std::find(fileNames.begin(),fileNames.end(),file)!=fileNames.end())
+        if(std::find(fileNames.begin(),fileNames.end(),file)!=fileNames.end()) // only export once
           return true; // not sure of true or false ... depends on program flow
         FILE* f = fopen(("single_export/" + file + ".obj").c_str(), "a");
         if (!f)
           return false;
-        // fputs("# File exported by ZenLib\n", f);
+        fileNames.emplace_back(file);
+        fputs("# File exported by ZenLib\n", f);
         fputs("",f);
         fputs(("o " + name + "\n").c_str(), f);
         // Write positions
@@ -76,10 +80,10 @@ namespace Utils
           fputs(("v " + std::to_string(v.Position.x) + " " + std::to_string(v.Position.y) + " " + std::to_string(-v.Position.z) + "\n").c_str(), f);
         // Write texture coords
         for (auto& v : mesh.vertices)
-          fputs(("v " + std::to_string(v.TexCoord.x) + " " + std::to_string(v.TexCoord.y) + "\n").c_str(), f);
+          fputs(("vt " + std::to_string(v.TexCoord.x) + " " + std::to_string(v.TexCoord.y) + "\n").c_str(), f);
         // Write normals
         for (auto& v : mesh.vertices)
-          fputs(("v " + std::to_string(v.Normal.x) + " " + std::to_string(v.Normal.y) + " " + std::to_string(v.Normal.z) + "\n").c_str(), f);
+          fputs(("vn " + std::to_string(v.Normal.x) + " " + std::to_string(v.Normal.y) + " " + std::to_string(v.Normal.z) + "\n").c_str(), f);
         fputs("s off\n", f);
         // Write faces
         for (auto& s : mesh.subMeshes) {
